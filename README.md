e59a799a72978ff0b0c9838d65dcc69e4e6a9fa3
mhasanjoy <m.hasanjoy13@gmail.com>
2021-12-22 14:59:16 +0600

    updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rest Countries
 
-This is a simple `TypeScript` project. It's created by using `React.js` & `Bootstrap`. All the countries information are shown here by using `REST COUNTRIES API`.
+This is a simple `TypeScript` practice project. It's created by using `React.js` & `Bootstrap`. All the countries information are shown here by using `REST COUNTRIES API`.
 
 ## [Live Site](https://rest-countries-lake.vercel.app/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import Country from './components/Country/Country';
+import React, { useEffect, useState } from "react";
+import Country from "./components/Country/Country";
 
 interface ICountry {
-  name: string;
-  flag: string;
-  alpha3Code: string;
-  capital: string;
+  name: {
+    common: string;
+  };
+  flags: {
+    png: string;
+  };
+  capital: string[];
   population: number;
   region: string;
   subregion: string;
@@ -16,9 +19,9 @@ function App() {
   const [spinner, setSpinner] = useState(true);
 
   useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
         setCountries(data);
         setSpinner(false);
       });
@@ -27,18 +30,17 @@ function App() {
   return (
     <main className="container">
       <h1 className="text-center m-5 pb-5">Countries</h1>
-      {
-        spinner &&
+      {spinner && (
         <section className="text-center m-5 p-5 fs-4">
-          <div className="spinner-border text-danger" role="status" style={{ width: '4rem', height: '4rem' }}>
+          <div className="spinner-border text-danger" role="status" style={{ width: "4rem", height: "4rem" }}>
             <span className="visually-hidden">Loading...</span>
           </div>
         </section>
-      }
+      )}
       <section className="row g-5">
-        {
-          countries.map(country => <Country key={country.alpha3Code} country={country} />)
-        }
+        {countries.map((country) => (
+          <Country key={country.name.common} country={country} />
+        ))}
       </section>
     </main>
   );
